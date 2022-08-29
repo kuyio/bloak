95e7b0e7d0ef39b8397091633e187142ccc1b2c0
Nicolas Bettenburg <nicbet@kuy.io>
2022-08-29 10:20:23 +0200

    feature: DRY out footer copyright and make configurable with engine

--- lib/bloak.rb
+++ lib/bloak.rb
@@ -3,10 +3,13 @@ require "bloak/engine"
 
 module Bloak
   mattr_accessor(:site_name)
+  mattr_accessor(:copyright)
+
   mattr_accessor(:admin_user)
   mattr_accessor(:admin_password)
 
   self.site_name = "The Blog"
+  self.copyright = "© 2022 #{Bloak.site_name} &mdash; All rights reserved.".html_safe
 
   def self.configure
     yield self
